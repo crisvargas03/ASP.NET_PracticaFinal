d4ceeedc4cd47f8a09cf61f915924516b6b58c35
luis mesi <guitarra2428@gmail.com>
2021-10-01 22:00:52 -0400

    Add Identity

---
+++ DotNetBanking/DotNetBanking/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DotNetBanking.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DotNetBanking.Areas.Identity.IdentityHostingStartup))]
+namespace DotNetBanking.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DotNetBankingContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DotNetBankingContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DotNetBankingContext>();
+            });
+        }
+    }
+}
