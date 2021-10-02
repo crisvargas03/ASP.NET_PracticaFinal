65a9cc062e78ebb68bcb0da1c059b6dcac94f0c2
Luis Aneuris Tavarez <luis_aneuris16@hotmail.com>
2021-10-01 20:57:36 -0400

    first commit

---
+++ DotNetBanking/Banking.Core/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking.Core
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string NO_Account { get; set; }
+        public string Type_Account { get; set; }
+        public double Amount { get; set; }
+        public DateTime DateCreate { get; set; }
+
+        //TODO:CREATE RELATION WITH CUSTOMER
+
+
+    }
+}
---
+++ DotNetBanking/Banking.Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banking.Core;
+
+
+namespace Banking.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Transactions> transactions { get; set; }
+        public DbSet<Account> accounts { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlite("Data Source=tienda.db");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
