a093c1ef933ab7fb876a7b4314dbe05203880b78
Luis Tavarez <luis_aneuris16@hotmail.com>
2021-10-12 14:56:16 -0400

    add merge with civargas

---
+++ DotNetBanking/DotNetBanking/Pages/Historico.cs
@@ -0,0 +1,25 @@
+﻿using Banking.Core;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DotNetBanking.Pages
+{
+    public class HistoricoViewModel : PageModel
+    {
+        private readonly Banking.Data.ApplicationDbContext _context;
+
+        public HistoricoViewModel(Banking.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Transaction> Transaction { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Transaction = await _context.transactions.ToListAsync();
+        }
+    }
+}
